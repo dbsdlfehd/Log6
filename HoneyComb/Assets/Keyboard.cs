5c6a18789ad7f25c878924b799fe0cb6b5fa94f1
GoldMan <142869347+RIKKA7061@users.noreply.github.com>
2024-09-28 19:23:26 +0900

    마크 5.0 (방1~6 추가함)

---
+++ HoneyComb/Assets/Keyboard.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keyboard : MonoBehaviour
+{
+	//플레이어의 좌표 변수 인듯?
+	public Transform player;
+
+	//좌표 변수 인듯?
+	public Transform[] Pos;
+	//Pos[0] 집 좌표
+	//Pos[1] room1 좌표
+	//Pos[2] room2 좌표
+	//Pos[3] room3 좌표...
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.H))
+		{
+			//플레이어 좌표를 -> 집 좌표
+			player.position = Pos[0].position;
+		}
+
+
+		//숫자키 = 방번호
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+			player.position = Pos[1].position;
+
+		if (Input.GetKeyDown(KeyCode.Alpha2))
+			player.position = Pos[2].position;
+
+		if (Input.GetKeyDown(KeyCode.Alpha3))
+			player.position = Pos[3].position;
+
+		if (Input.GetKeyDown(KeyCode.Alpha4))
+			player.position = Pos[4].position;
+
+		if (Input.GetKeyDown(KeyCode.Alpha5))
+			player.position = Pos[5].position;
+
+		if (Input.GetKeyDown(KeyCode.Alpha6))
+			player.position = Pos[6].position;
+	}
+}
