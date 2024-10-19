4d715b03f1549713361053839a655ef91c0646f7
GoldMan <142869347+RIKKA7061@users.noreply.github.com>
2024-10-20 03:00:44 +0900

    마크 20.0
    회차별 대화 횟수 추가 (이제 죽음 회차별이 아니라 회차별 대화 횟수를 통해서 대화를 볼수 있습니다.)

--- HoneyComb/Assets/TalkManager.cs
+++ HoneyComb/Assets/TalkManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -18,10 +19,16 @@ public class TalkManager : MonoBehaviour
 	public TextMeshProUGUI Talker;
 	public TextMeshProUGUI Dialog;
 
+	[Header("회차별 대화 횟수")]
+	public TextMeshProUGUI TalkCountNum;
+
 	string talker = "";
 	string dialog = "";
 	public int tempId = 0;
 	public int tempDeadCount = 0;
+	public int TalkCount = 0;
+	bool isDeadUp = false;
+	bool isUpGraded = false;
 
 	public bool isOverIndex = false;
 	//대화 액션 함수
@@ -39,22 +46,39 @@ public class TalkManager : MonoBehaviour
 		//스캔된 오브젝트의 아이디와 NPC여부를 Talk함수에 보냅니다.
 		Talk(obj.id, obj.isNPC);
 		tempId = obj.id;
-
 	}
 
 	private void Update()
 	{
+		//UI에 나타내기
 		Talker.text = talker.ToString();
 		Dialog.text = dialog.ToString();
+		TalkCountNum.text = "회차별 대화 횟수 : " + TalkCount.ToString();
 	}
 
 	public int i = 0;
 	void Talk(int id, bool isNPC)
 	{
 		//죽음 횟수가 바뀌었다?
-		if(tempDeadCount != 0 && tempDeadCount != player.DeadCount)
+		if (tempDeadCount != 0 && tempDeadCount != player.DeadCount)
 		{
 			i = 0;
+			isDeadUp = true;//
+		}
+
+		//만일 대화를 클릭했다면?
+		if (player.DeadCount > TalkCount && isUpGraded == false)
+		{
+			isUpGraded = true;
+			TalkCount++;
+		}
+
+		//만약 죽음 횟수보다 회차별 대화 횟수가 적다면
+		if (player.DeadCount > TalkCount && isDeadUp)
+		{
+			TalkCount++;
+			//Debug.Log($"회차별 대화 횟수 : {TalkCount}");
+			isDeadUp = false;
 		}
 
 		//다른사람 대화했다가 저사람 대화했다가 순번 꼬이는 일 방지
@@ -63,10 +87,14 @@ public class TalkManager : MonoBehaviour
 			i = 0;
 		}
 
+		//현재 죽음 횟수 임시 저장
 		tempDeadCount = player.DeadCount;
+
+		//중요!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		//저장된 대화 목록중에 해당 대화 번호의 대화말 가져오기
 		if (isOverIndex == false)
 		{
-			int DialogNum = player.DeadCount * 10000 + id + i;//대화번호
+			int DialogNum = TalkCount * 10000 + id + i;//대화번호
 			i++;
 			string[] texts = parsingManager.GetDialogPlz(DialogNum);
 			talker = texts[1];
