ef068f1153b37eb8088192730a703fac7cc8a7df
GoldMan <142869347+RIKKA7061@users.noreply.github.com>
2024-10-26 01:32:37 +0900

    마크 23.0
    NPC별로 회차별 대화 횟수가 저장하게 구현함

--- HoneyComb/Assets/02.Scripts/_Object.cs
+++ HoneyComb/Assets/02.Scripts/_Object.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class _Object : MonoBehaviour
 {
-    public int id;
-    public bool isNPC;
+    public int id;//NPC번호
+
+    //public bool isNPC;//NPC인지 확인하는 건데 필요 없는듯
+
+    //NPC별 횟차별 대화 횟수
+    public int EachTalkCount = 0;
+    public int TempEachTalkCount = 0;
+
+    //테스트용 이름 변수
+    public new string name;
+
+    //NPC별 죽은뒤 대화 확인용 bool형 변수
+	public bool isDeadUp = false;
+	public bool isUpGraded = false;
+
+    public int PlayerDead;
+    public int tempPlayerDead = 0;
 }
--- HoneyComb/Assets/TalkManager.cs
+++ HoneyComb/Assets/TalkManager.cs
@@ -1,106 +1,96 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
+using TMPro;//TextMeshPro용 namespace
 using UnityEngine;
 
 public class TalkManager : MonoBehaviour
 {
-	[Header("얘는 안갖다 붙여도 됨")]
+	/*TalkManger란 대화 관련된 것을 처리하는 스크립트입니다.*/
+
+	[Header("NPC 스캔")]
 	public GameObject ScanObject;
 
-	[Header("플레이어 스크립트")]
+	[Header("Player")]
 	public Player player;
 
-	[Header("파씽매니저")]
+	[Header("파씽")]
 	public ParsingManager parsingManager;
 
-	[Header("대화창")]
+	[Header("DialogSet")]
 	public TextMeshProUGUI Talker;
 	public TextMeshProUGUI Dialog;
-
-	[Header("회차별 대화 횟수")]
-	public TextMeshProUGUI TalkCountNum;
-
 	string talker = "";
 	string dialog = "";
-	public int tempId = 0;
-	public int tempDeadCount = 0;
-	public int TalkCount = 0;
-	bool isDeadUp = false;
-	bool isUpGraded = false;
 
-	public bool isOverIndex = false;
-	//대화 액션 함수
-	public void DialogAction(GameObject scannedObject)
-    {
-		//스캔된 오브젝트
-		ScanObject = scannedObject;
-		
-		//스캔된 오브젝트를 컴포넌트에 가져옵니다.
-		_Object obj = scannedObject.GetComponent<_Object>();
-		
-		//스캔된 오브젝트의 아이디와 NPC여부를 확인합니다.
-		//Debug.Log(obj.id + " " + obj.isNPC);
+	[Header("테스트")]
+	public TextMeshProUGUI TalkCountNum;
+	string text = "";
+	string NPCname;//테스트용 NPC 이름 변수
 
-		//스캔된 오브젝트의 아이디와 NPC여부를 Talk함수에 보냅니다.
-		Talk(obj.id, obj.isNPC);
+	int tempId = 0;//임시 NPC 주민등록번호
+	//int tempDeadCount = 0;//임시 Player죽음 횟수
+
+	public bool isOverIndex = false;//대화 흐름수 EX. 10100~10107
+
+	public void DialogAction(GameObject scannedObject)//진우가 한번 스캔시 작동
+	{
+		ScanObject = scannedObject;//진우가 스캔한 NPC
+
+		_Object obj = scannedObject.GetComponent<_Object>();//NPC 정보
+
+		Talk(obj.id, scannedObject);
 		tempId = obj.id;
+		NPCname = obj.name;
+		text = NPCname + "의 회차별 대화 횟수 : " + obj.EachTalkCount.ToString();//test
 	}
 
 	private void Update()
 	{
-		//UI에 나타내기
-		Talker.text = talker.ToString();
-		Dialog.text = dialog.ToString();
-		TalkCountNum.text = "회차별 대화 횟수 : " + TalkCount.ToString();
+		Talker.text = talker.ToString();//DialogSet
+		Dialog.text = dialog.ToString();//DialogSet
+		TalkCountNum.text = text;//test
 	}
 
 	public int i = 0;
-	void Talk(int id, bool isNPC)
+	void Talk(int id, GameObject scannedObject)
 	{
-		//죽음 횟수가 바뀌었다?
-		if (tempDeadCount != 0 && tempDeadCount != player.DeadCount)
+		_Object obj = scannedObject.GetComponent<_Object>();
+
+		if(tempId != id)
+		{
+			Debug.Log("B 나는 1 늘어나");
+		}
+
+		if (obj.tempPlayerDead != player.DeadCount)
 		{
 			i = 0;
-			isDeadUp = true;//
+			obj.isDeadUp = true;
 		}
 
-		//만일 대화를 클릭했다면?
-		if (player.DeadCount > TalkCount && isUpGraded == false)
+		if (player.DeadCount > obj.EachTalkCount && obj.isDeadUp == true)
 		{
-			isUpGraded = true;
-			TalkCount++;
+			obj.EachTalkCount++;
+			obj.isDeadUp = false;
 		}
 
-		//만약 죽음 횟수보다 회차별 대화 횟수가 적다면
-		if (player.DeadCount > TalkCount && isDeadUp)
-		{
-			TalkCount++;
-			//Debug.Log($"회차별 대화 횟수 : {TalkCount}");
-			isDeadUp = false;
-		}
-
-		//다른사람 대화했다가 저사람 대화했다가 순번 꼬이는 일 방지
-		if(tempId != 0&& tempId != id)
+		if (tempId != 0 && tempId != id)//다른사람 대화했다가 저사람 대화했다가 순번 꼬이는 일 방지
 		{
 			i = 0;
 		}
 
-		//현재 죽음 횟수 임시 저장
-		tempDeadCount = player.DeadCount;
+		obj.tempPlayerDead = player.DeadCount;//현재 죽음 횟수 임시 저장
 
-		//중요!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		//저장된 대화 목록중에 해당 대화 번호의 대화말 가져오기
-		if (isOverIndex == false)
+		if (isOverIndex == false)//대화 순번 index를 넘지 않을 때
 		{
-			int DialogNum = TalkCount * 10000 + id + i;//대화번호
+			int DialogNum = obj.EachTalkCount * 10000 + id + i;//대화번호
+			Debug.Log(DialogNum);//테스트
 			i++;
 			string[] texts = parsingManager.GetDialogPlz(DialogNum);
 			talker = texts[1];
 			dialog = texts[0];
 		}
-		else if(isOverIndex == true)
+		else if (isOverIndex == true)//대화 순번 index를 넘을 때
 		{
 			i = 0;
 			isOverIndex = false;
